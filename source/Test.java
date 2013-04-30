049fc94eed3755e89bf88d48c232602d427be86b
Andrew Ghobrial <awghobrial@gmail.com>
2013-04-30 01:18:10 -0400

    commit

---
+++ source/Test.java
@@ -0,0 +1,8 @@
+
+public class Test {
+	public static void main(String[] args){
+	JoinStrings joiner = new JoinStrings();
+	String s = joiner.join("Andrew","Wadie","Ghobrial");
+	System.out.println(s);
+	}
+}
